da26be2f8237752d8a0eff1482fcfd16e0e2d8ca
samnags <sam.nagourney@gmail.com>
2017-01-05 10:22:25 -0500

    logo centered

--- src/containers/app.js
+++ src/containers/app.js
@@ -3,13 +3,22 @@ import { connect } from 'react-redux'
 import GuestHeader from '../components/guest_header'
 import LoggedInHeader from '../components/logged_in_header'
 import '../../public/App.css'
+import logo from '../../images/HelloPantryRound_T.png'
+import { Grid, Row, Col } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
       <div>
-        {this.props.logged_in ? <LoggedInHeader /> : <GuestHeader /> }
+        {/* <Grid> */}
+        {/* {this.props.logged_in ? <LoggedInHeader /> : <GuestHeader /> } */}
         {this.props.children}
+        {/* <Row> */}
+          {/* <Col sm={6} md={3}> */}
+          <img src={logo} alt="Logo" className="logo" />
+          {/* </Col>
+        </Row>
+        </Grid> */}
       </div>
     );
   }
