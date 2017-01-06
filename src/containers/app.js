53accdf9319fcb7e8c07b3cb6926946f1c7ccd22
samnags <sam.nagourney@gmail.com>
2017-01-06 16:24:47 -0500

    sign up responsive without login button

--- src/containers/app.js
+++ src/containers/app.js
@@ -4,27 +4,32 @@ import GuestHeader from '../components/guest_header'
 import LoggedInHeader from '../components/logged_in_header'
 import '../../public/App.css'
 import logo from '../../images/HelloPantryRound_T.png'
-// import { Grid, Row, Col } from 'react-bootstrap'
+// import { Grid, Row, Col, Image } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         {/* {this.props.logged_in ? <LoggedInHeader /> : <GuestHeader /> } */}
         {this.props.children}
+
         <div>
           <img src={logo} alt="Logo" className="logo" />
         </div>
-        <div className="login-button" >
+
+        {/* <div className="login-button" >
           <div className="button-text">
             LOG IN
           </div>
-        </div>
-        <div className="signup-button">
-          <div className="button-text">
-            SIGN UP
+        </div> */}
+
+          <div className="signup-button">
+            <div className="button-text">
+              SIGN UP
+            </div>
           </div>
-        </div>
+
+
       </div>
     );
   }
