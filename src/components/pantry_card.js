224b08811aaf7358c631f93b9fd5b5626866f033
samnags <sam.nagourney@gmail.com>
2017-01-11 22:18:51 -0500

    box ready. needs date reformatting and to get multiple pantry boxs per line

--- src/components/pantry_card.js
+++ src/components/pantry_card.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router'
 import '../../public/app.css'
 import { Col } from 'react-bootstrap';
 
-const PantryCard = (props) => {
+const PantryCard = (props) => {  
     let pantry = props.pantry
     return (
       <div className="pantrycard">
         <div className="section group">
           <div className=".span-1-of-4">
-            {pantry.location}
+            <div className="pantryname">{pantry.location}</div>
+            <div className="updated pink chalkboard">Last updated at {pantry.updated_at}</div>
           </div>
         </div>
       </div>
