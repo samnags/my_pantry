4eaad9cfa0b95c1b8ce6404260dc3c2154219411
samnags <sam.nagourney@gmail.com>
2017-01-11 22:27:16 -0500

    button adjustment

--- src/components/pantry_card.js
+++ src/components/pantry_card.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router'
 import '../../public/app.css'
 import { Col } from 'react-bootstrap';
 
-const PantryCard = (props) => {  
+const PantryCard = (props) => {
     let pantry = props.pantry
     return (
       <div className="pantrycard">
         <div className="section group">
           <div className=".span-1-of-4">
-            <div className="pantryname">{pantry.location}</div>
+            <div className="pantryname"><Link to={`/pantries/${pantry.id}`} key={pantry.id}>{pantry.location}</Link></div>
             <div className="updated pink chalkboard">Last updated at {pantry.updated_at}</div>
           </div>
         </div>
