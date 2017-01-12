42d088a032438b8afdfb54f7b1ce422cda4b3f3f
samnags <sam.nagourney@gmail.com>
2017-01-11 22:01:01 -0500

    added bottom margin to header

--- src/components/pantry_card.js
+++ src/components/pantry_card.js
@@ -6,16 +6,24 @@ import { Col } from 'react-bootstrap';
 const PantryCard = (props) => {
     let pantry = props.pantry
     return (
-      <Col md={4}>
-      <div className='card'>
-          <div className="card-block">
-            <h4 className="card-title"><Link to={`/pantries/${pantry.id}`} key={pantry.id}>{pantry.location}</Link></h4>
-            <p className="card-text">Some stuff</p>
+      <div className="pantrycard">
+        <div className="section group">
+          <div className=".span-1-of-4">
+            {pantry.location}
           </div>
-
+        </div>
       </div>
-      </Col>
     )
 }
 
 module.exports = PantryCard
+
+{/* <Col md={4}>
+  <div className='card'>
+    <div className="card-block">
+      <h4 className="card-title"><Link to={`/pantries/${pantry.id}`} key={pantry.id}>{pantry.location}</Link></h4>
+      <p className="card-text">Some stuff</p>
+    </div>
+
+    </div>
+</Col> */}
