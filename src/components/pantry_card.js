2a53c6c8e10ed92808bbafaa148325a278703280
samnags <sam.nagourney@gmail.com>
2017-01-11 23:00:42 -0500

    changed date format

--- src/components/pantry_card.js
+++ src/components/pantry_card.js
@@ -3,11 +3,14 @@ import { Link } from 'react-router'
 
 const PantryCard = (props) => {
     let pantry = props.pantry
+    let date = new Date(pantry.updated_at)
+    let formattedDate = date.toLocaleDateString()
+
     return (
       <div className="pantrycard col span-1-of-4">
             <div className="pantryname"><Link to={`/pantries/${pantry.id}`} key={pantry.id}>{pantry.location}</Link></div>
             {/* <div className="chalkboard">Pantry</div> */}
-            <div className="updated pink chalkboard">last updated at {pantry.updated_at}</div>
+            <div className="updated pink chalkboard">last updated at {formattedDate}</div>
       </div>
     )
 }
